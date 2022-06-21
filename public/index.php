36c5a4ee620495ac3322d228f2e2e4b059d338b2
Luciano Bertran <bertranluciano@gmail.com>
2022-06-20 22:28:03 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,15 @@
-hello
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Challenge Ingreso Career Switch: DeFI-Solidity</title>
+</head>
+<body>
+    <h1>Challenge Ingreso Career Switch: DeFI-Solidity</h1>
+    <h3>Modo de uso de la API</h3>
+    <p>Hacer una petición de tipo GET a https://challenge-ingreso-lbertran.herokuapp.com/api.php?email=mail-de-prueba</p>
+    <p>donde mail-de-prueba debe ser reemplazado por el mail que se quiera corroborar.</p>
+</body>
+</html>
